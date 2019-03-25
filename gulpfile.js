62ca39f339ec7a6049b5311fac8d962bb3a94af0
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-03-25 19:44:03 -0400

    gulp style added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-const gulp = require("gulp");
+
+
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
 
 gulp.task("default", function() {
   // code for your default task goes here
   console.log("Shehryar is running Gulp");
+});
+
+gulp.task("styles", function(){
+  gulp.src('sass/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./css'));
 });
